3068bb2f8324a478687c6c9b69707e398b1ba970
Xero1000 <alex.heil@comcast.net>
2023-10-25 09:23:15 -0400

    Implement custom font

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,8 @@ export default async function Home() {
   
   return (
     <main className='relative h-screen'>
-      <Image 
+      <h1>Hello World</h1>
+      {/* <Image 
         src="https://bit.ly/react-cover" 
         alt="Nebula"
         fill
@@ -17,7 +18,7 @@ export default async function Home() {
         sizes='(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw'
         quality={100}
         priority
-      />
+      /> */}
     </main>
   )
 }
