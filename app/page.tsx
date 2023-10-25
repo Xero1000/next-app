f3f2df9ecef090aa3e22443b56442f1b383bf579
Xero1000 <alex.heil@comcast.net>
2023-10-25 08:59:11 -0400

    Add optimized image

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,23 @@
 import Image from 'next/image'
+import aldebaran from '@/public/images/aldebaran-s-qtRF_RxCAo0-unsplash.jpg'
 import Link from 'next/link'
 import ProductCard from './components/ProductCard'
 import { getServerSession } from 'next-auth'
 import { authOptions } from './api/auth/[...nextauth]/route'
 
 export default async function Home() {
-  const session = await getServerSession(authOptions)
   
   return (
-    <main>
-      <h1>Hello { session && <span>{session.user!.name}</span>}</h1>
-      <Link href='/users'>Users</Link>
-      <ProductCard />
+    <main className='relative h-screen'>
+      <Image 
+        src="https://bit.ly/react-cover" 
+        alt="Nebula"
+        fill
+        className='object-cover'
+        sizes='(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw'
+        quality={100}
+        priority
+      />
     </main>
   )
 }
